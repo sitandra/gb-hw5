3ccb69bfe86f85fe318d7ef1c8b53f6362ab7621
sitandra <u.deb@mail.ru>
2023-08-20 12:46:35 +0500

    Initial Commit

---
+++ gb-hw5/gb_hw5App.swift
@@ -0,0 +1,17 @@
+//
+//  gb_hw5App.swift
+//  gb-hw5
+//
+//  Created by Юлия Дебелова on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct gb_hw5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
